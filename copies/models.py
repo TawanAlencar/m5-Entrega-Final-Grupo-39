824aacfc0ad8f5af66667184e27c46f082974951
TawanAlencar <josetawan300@gmail.com>
2023-03-06 15:04:10 -0300

    Add: Models

---
+++ copies/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Copy(models.Model):
+    is_lending = models.BooleanField(default=False)
+    book = models.ManyToManyField("books.Book", related_name="copies")
+
+
+class Lending(models.Model):
+    is_date = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="lending"
+    )
+    copy = models.ForeignKey(
+        "copies.Copy", on_delete=models.CASCADE, related_name="lending"
+    )
