965b23017c8793996951ef0e5bb01e57b2c4ec15
Jhonatta Martins da Silva <jhonatta_martins@hotmail.com>
2023-03-14 04:30:10 -0300

    fix: Bug email

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Book, Follow
 from copies.models import Copy
+from users.models import User
 from rest_framework.serializers import ValidationError
 from .serializers import BookSerializer, FollowSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -44,14 +45,17 @@ class FollowBook(ListCreateAPIView):
         book = get_object_or_404(Book, id=self.kwargs.get("book_id"))
         copy_data = Copy.objects.filter(id=self.kwargs.get("book_id"))
         copy_is_leding = copy_data.last()
+        user = get_object_or_404(User, id=self.kwargs.get("user_id"))
+        email_user = user.email
         
         send_mail('Você seguiu este livro', 
                   f'Nome do livro: {book.title} \nDescrição: {book.description}\nEmprestado:{copy_is_leding.is_lending}',
-                  settings.EMAIL_HOST_USER, ['jhonatta_martins@hotmail.com'], 
+                  settings.EMAIL_HOST_USER, [email_user], 
                   False)
-                  
+
         if Follow.objects.filter(book=self.kwargs.get("book_id"),user_id=self.request.user.id):
             raise ValidationError("book is already being followed")
+       
         return serializer.save(
             book_id=self.kwargs.get("book_id"), user_id=self.request.user.id
         )
