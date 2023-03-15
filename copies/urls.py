8c8fd862a9a062dc6797a6af03800911528838b3
Lucas Rodrigues <lucasvplayer@gmail.com>
2023-03-15 12:09:40 -0300

    Add: Change on lending route

--- copies/urls.py
+++ copies/urls.py
@@ -3,7 +3,7 @@ from .views import Copyview, LendingView, DestroyLendingView, ListLendingStudant
 
 urlpatterns = [
     path("books/<int:book_id>/copy/", Copyview.as_view()),
-    path("lending/<int:copy_id>/", LendingView.as_view()),
+    path("lending/<int:copy_id>/<int:studants_id>/", LendingView.as_view()),
     path("listlending/<int:studants_id>/", ListLendingStudants.as_view()),
     path("lending/<int:lending_id>/delete/", DestroyLendingView.as_view()),
 ]
